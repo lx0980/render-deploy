07e8c0203cbc7dcde6a0c8ffb43fe56fccd2d052
lx0980 <138602811+lx0980@users.noreply.github.com>
2025-10-06 22:51:34 +0530

    Update projects.py

--- projects.py
+++ projects.py
@@ -1,7 +1,7 @@
 PROJECTS = {
     "MyApp1": {
-        "app_url": "https://myapp1.onrender.com",
-        "deploy_url": "https://api.render.com/deploy/somehook1"
+        "app_url": "https://myapp1.onrender.com", # app url
+        "deploy_url": "https://api.render.com/deploy/somehook1" #Hook Url For deploy
     },
     "MyApp2": {
         "app_url": "https://myapp2.onrender.com",
